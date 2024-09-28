99025b0572e0517361451df5e8d7fec9e6c22b26
Konrad Kalemba <konrad_kal@hotmail.com>
2024-09-28 16:04:42 +0200

    allow single file upload

--- frontend/components/dropzone.tsx
+++ frontend/components/dropzone.tsx
@@ -5,35 +5,44 @@ import { BorderBeam } from "./ui/border-beam";
 import { VideoThumbnail } from "./video-thumbnail";
 
 export function Dropzone({ children }: { children: React.ReactNode }) {
-  const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
+  const { acceptedFiles, getRootProps, getInputProps } = useDropzone({
+    multiple: false,
+    accept: {
+      "video/*": [".mp4", ".mov", ".avi", ".mkv"],
+    },
+  });
 
   return (
     <div
       {...getRootProps({
         className:
-          "bg-neutral-100 rounded-xl h-[200px] flex flex-col items-center justify-center w-2/3 relative shadow border border-neutral-200",
+          "bg-neutral-100 rounded-xl min-h-[200px] flex p-2 flex-col items-center justify-center w-2/3 relative shadow border border-neutral-200",
       })}
     >
       <BorderBeam />
       <input {...getInputProps()} />
 
-      <div className="absolute left-2 top-2 grid grid-cols-6 gap-2">
-        {acceptedFiles.map((file) => (
-          <div
-            key={file.name}
-            className="bg-neutral-200 rounded-2xl flex items-center shadow-sm relative"
-          >
-            <VideoThumbnail file={file} />
-            <div className="absolute top-1 max-w-[80%] left-1/2 -translate-x-1/2 bg-neutral-900/60 rounded-full px-3 text-xs backdrop-blur-lg text-white/90 font-medium h-5 flex items-center line-clamp-1 justify-center text-ellipsis overflow-hidden">
-              {file.name}
+      {acceptedFiles.length > 0 && (
+        <div className="w-full flex items-center justify-center gap-2">
+          {acceptedFiles.map((file) => (
+            <div
+              key={file.name}
+              className="bg-neutral-200 rounded-2xl w-1/2 flex items-center shadow-sm relative"
+            >
+              <VideoThumbnail file={file} />
+              <div className="absolute top-2 max-w-[80%] left-1/2 -translate-x-1/2 bg-neutral-900/50 rounded-full px-3 text-xs backdrop-blur-xl text-white/90 font-medium h-5 flex items-center line-clamp-1 justify-center text-ellipsis overflow-hidden">
+                {file.name}
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      )}
 
-      <p className="text-neutral-500 text-sm font-medium">
-        Przeciągnij i upuść plik wideo, lub kliknij, aby wybrać plik
-      </p>
+      {acceptedFiles.length === 0 && (
+        <p className="text-neutral-500 text-sm font-medium">
+          Przeciągnij i upuść plik wideo, lub kliknij, aby wybrać plik
+        </p>
+      )}
 
       {children}
     </div>
